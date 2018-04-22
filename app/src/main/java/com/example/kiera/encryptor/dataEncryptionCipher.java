b5c91cd08619665c5bb78d338b52d5a25e79811b
KieranABrown <Kieranbrowna@hotmail.co.uk>
2018-04-22 22:40:06 +0100

    Android Studio 3.1.1 Update
    Pushing update of program following Android Studio 3.1.1 update.
    Additionally DES files have also altered.

--- app/src/main/java/com/example/kiera/encryptor/dataEncryptionCipher.java
+++ app/src/main/java/com/example/kiera/encryptor/dataEncryptionCipher.java
@@ -1,7 +1,11 @@
 package com.example.kiera.encryptor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class dataEncryptionCipher extends AppCompatActivity {
 
@@ -10,4 +14,34 @@ public class dataEncryptionCipher extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_encryption_cipher);
     }
+
+    public void MoveEncrypt(View view){
+        Intent i = new Intent(this, Encrypted.class);
+
+        // get the text box
+        EditText inputText = findViewById(R.id.inputText);
+        //Gets the text which is in the box
+        String txtData = inputText.getText().toString();
+        //Gets the length of the text in the edit text box
+        int txtValue = inputText.getText().toString().length();
+
+        //Ensures that there is text entered before button can be pressed.
+        if (txtValue == 0){
+            // Display message to user telling that they need to add text to the box.
+            Toast.makeText(this, "You did not enter any text", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Assign the new text the encrypted to a new variable
+        String encrypted = DES(txtData);
+
+        //add the new intent with the
+        i.putExtra("txtData", encrypted);
+        startActivity(i);
+    }
+
+    static String DES(String txtData){
+        return new String(txtData);
+    }
 }
+
